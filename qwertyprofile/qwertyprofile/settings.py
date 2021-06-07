c521cc04af08c250200ace0365a74462f8de5e67
phylynxbelt <thanaporn.jira@odds.team>
2021-06-07 15:09:21 +0700

    feat: add dotenv for project

--- qwertyprofile/qwertyprofile/settings.py
+++ qwertyprofile/qwertyprofile/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)8%%bh*hv1ha(d(u@n&jx=xgnw7vv9zec4840t+%&p06awf#ym'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
