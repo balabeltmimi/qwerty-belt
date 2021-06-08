56cca5aa09432ab9dcdd975b00ce50b893c8dc70
phylynxbelt <thanaporn.jira@odds.team>
2021-06-08 21:40:01 +0700

    feat: add test for required field

--- qwertyprofile/qwertyindex/tests.py
+++ qwertyprofile/qwertyindex/tests.py
@@ -1,3 +1,21 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+from qwertyindex.models import qwertyindex
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def test_profile_should_have_defined_fields(self):
+        profile = qwertyindex.objects.create(
+            name='Beltrt',
+            github_url='https://github.com/balabeltmimi',
+            github_project_url='https://github.com/balabeltmimi/qwerty-belt',
+        )
+
+        assert profile.name == 'Beltrt'
+        assert profile.github_url == 'https://github.com/balabeltmimi'
+        assert profile.github_project_url == 'https://github.com/balabeltmimi/qwerty-belt'
+
+class TestIndexView(TestCase):
+    def test_index_view_should_be_accessible(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
