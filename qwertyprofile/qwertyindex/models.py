ec30f62c896f83e59f61fb9f2317aa4b74af8248
phylynxbelt <thanaporn.jira@odds.team>
2021-06-08 20:12:05 +0700

    refactor: refactor model and model type

--- qwertyprofile/qwertyindex/models.py
+++ qwertyprofile/qwertyindex/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class qwertyindex(models.Model):
     name = models.CharField(max_length=100)
-    github_url = models.CharField(max_length=200)
-    github_project_url = models.CharField(max_length=300)
+    github_url = models.URLField(max_length=200)
+    github_project_url = models.URLField(max_length=300)
 
 class Subscibemodel(models.Model):
 
