ec3773c6b13b1fb4349a33d886d1d2b02ec24558
phylynxbelt <thanaporn.jira@odds.team>
2021-06-08 21:51:30 +0700

    test: add test for view page

--- qwertyprofile/qwertyindex/tests.py
+++ qwertyprofile/qwertyindex/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from qwertyindex.models import qwertyindex
+from qwertyindex.models import qwertyindex, Subscibemodel
+
 
 # Create your tests here.
 class TestProfile(TestCase):
@@ -15,7 +16,22 @@ class TestProfile(TestCase):
         assert profile.github_url == 'https://github.com/balabeltmimi'
         assert profile.github_project_url == 'https://github.com/balabeltmimi/qwerty-belt'
 
+
 class TestIndexView(TestCase):
-    def test_index_view_should_be_accessible(self):
+    def test_index_view_should_see_my_name(self):
+        # Given
+        qwertyindex.objects.create(name='Belt')
+        # When
         response = self.client.get('/')
+        # Then
         assert response.status_code == 200
+        assert 'Belt' in str(response.content)
+
+    def test_index_view_should_save_subscriber_email_when_input_form(self):
+        qwertyindex.objects.create(name='Belt')
+        data = {
+            'email': 'thanaporn.jira@odds.team'
+        }
+        response = self.client.post('/', data=data)
+        sub_email = Subscibemodel.objects.last()
+        assert sub_email.email == 'thanaporn.jira@odds.team'
